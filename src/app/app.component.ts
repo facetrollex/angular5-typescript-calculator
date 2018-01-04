2aff3741e737222e73c294543c92318c477302b6
facetrollex <alexey.khamitsevich@gmail.com>
2018-01-04 17:48:49 +0300

    Basic functionality without styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'app';
+  public result: string = '';
+
+  public memory: String[] = [];
+
+  readonly calcMap: Object[] = [
+    [7, 8, 9, '*'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '+'],
+    [0, '.', '/', '='],
+  ];
+
+  public addToCalculation(param: any): void {
+    switch (param) {
+      case '=':
+        this.result = <boolean><any>this.result.length ? this.calculate() : '';
+        break;
+      case '.':
+        if (!isNaN(+this.result[this.result.length - 1])) {
+          this.result += param;
+        }
+        break;
+      default:
+        this.result += param;
+        break;
+    }
+  }
+
+  private calculate(): string {
+    let result: string;
+
+    try {
+      result = eval(this.result).toString();
+    } catch (e) {
+      result = 'Syntax Error';
+    }
+
+    return result;
+  }
+
+  public delete(last?: boolean): void {
+    this.result = last ? this.result.slice(0, -1) : '';
+  }
+
+  public saveToMemory(): void {
+    if (!isNaN(+this.result)) {
+      this.memory.push(this.result);
+    }
+  }
+
+  public copyFromMemory(item: string): void {
+    this.result = item;
+  }
+
+  public deleteFromMemory(index: number): void {
+    this.memory.splice(index, 1);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
-
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 
 
@@ -10,9 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent,
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
